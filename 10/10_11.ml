b8311cac722cdc6748bf9358b1ab8edb777ef632
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-08-22 00:11:04 +0900

    Livecoding...

--- 10/10_11.ml
+++ 10/10_11.ml
@@ -5,15 +5,15 @@
 (* #use "../common/eki.ml" *)
 
 (* 目的: 直接接続されている2駅の距離を取得する *)
-(* get_ekikan_kyori string -> string -> ekikan_t list -> float *)
-let rec get_ekikan_kyori s1_kanji s2_kanji ekikan_list = match ekikan_list with
+(* get_ekikan_kyori ekikan_t list -> string -> string -> float *)
+let rec get_ekikan_kyori ekikan_list s1_kanji s2_kanji = match ekikan_list with
       [] -> infinity
     | {kiten=k; shuten=s; kyori=d} :: rest -> 
         if s1_kanji = k && s2_kanji = s || s1_kanji = s && s2_kanji = k
             then d
-            else get_ekikan_kyori s1_kanji s2_kanji rest
+            else get_ekikan_kyori rest s1_kanji s2_kanji
 
 (* tests *)
-let t1 = get_ekikan_kyori "茗荷谷" "新大塚" global_ekikan_list = 1.2
-let t2 = get_ekikan_kyori "新大塚" "茗荷谷" global_ekikan_list = 1.2
-let t3 = get_ekikan_kyori "茗荷谷" "池袋" global_ekikan_list = infinity
+(* let t1 = get_ekikan_kyori "茗荷谷" "新大塚" global_ekikan_list = 1.2 *)
+(* let t2 = get_ekikan_kyori "新大塚" "茗荷谷" global_ekikan_list = 1.2 *)
+(* let t3 = get_ekikan_kyori "茗荷谷" "池袋" global_ekikan_list = infinity *)
