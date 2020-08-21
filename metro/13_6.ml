4aa96cef90540482122af9c58b00e02970a3ba9c
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-08-22 01:19:02 +0900

    14.13

--- metro/13_6.ml
+++ metro/13_6.ml
@@ -21,5 +21,21 @@ let update from_station lst =
     in List.map (update1 from_station) lst
 
 (* tests *)
-let t2_1 = update yoyogiuehara [] = []
-let t2_2 = update yoyogiuehara [yoyogikouen; meijijinguumae] = [yoyogikouen_updated; meijijinguumae]
+let t1_1 = update yoyogiuehara [] = []
+let t1_2 = update yoyogiuehara [yoyogikouen; meijijinguumae] = [yoyogikouen_updated; meijijinguumae]
+
+(* 14.13 *)
+(* 無名関数ver. *)
+(* 目的: 基点となる駅と駅のリストを受け取り、更新されたリストを返す *)
+let update_with_lambda from_station lst = 
+    List.map
+        (fun to_station ->
+            let ekikan_kyori = get_ekikan_kyori global_ekikan_list from_station.name to_station.name in
+            let new_distance = from_station.shortest_distance_meter +. ekikan_kyori in
+                if ekikan_kyori != infinity && new_distance < to_station.shortest_distance_meter
+                    then {name=to_station.name; shortest_distance_meter=new_distance; path=to_station.name :: from_station.path}
+                    else to_station)
+        lst
+
+let t2_1 = update_with_lambda yoyogiuehara [] = []
+let t2_2 = update_with_lambda yoyogiuehara [yoyogikouen; meijijinguumae] = [yoyogikouen_updated; meijijinguumae]
